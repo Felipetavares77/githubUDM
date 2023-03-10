5299b1659df22756f9aa86e7347d43cce5318bec
Felipe Tavares <felipetavares1990@gmail.com>
2023-03-10 14:52:52 +0100

    Added Constructor and Overcharge

--- src/application/StockProduct.java
+++ src/application/StockProduct.java
@@ -11,26 +11,32 @@ public class StockProduct {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product();
+		
 		System.out.println("Enter the product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price, quantity);
+		
+		//product.setName("Computer");
+		//product.setPrice(1000.00);
+		System.out.println("Updated data: " + product.getName()+ " " + product.getPrice() + " " + product.getQuantity());
 		
 		System.out.println(product);
 		System.out.println();
 		
 		System.out.println("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		product.addProducts(quantity);
 		System.out.println();
 		System.out.println("Updated data: " + product);
 		
 		System.out.println("Enter the number of products to be removed from stock: ");
-		product.quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		product.excludeProducts(quantity);
 		System.out.println();
 		System.out.println("Updated data: " + product);
